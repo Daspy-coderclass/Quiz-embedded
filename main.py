0203de5c408c9b3cf81065912fbb289063b9e61a
Daspy-coderclass <77778303+Daspy-coderclass@users.noreply.github.com>
2021-01-22 09:34:50 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,71 @@
 def on_button_pressed_a():
-    global vraag
-    vraag += 1
-input.on_button_pressed(Button.A, on_button_pressed_a)
+    global speler, Antwoord
+    while begin:
+        speler = A
+    Antwoord = "A"
 
+def on_button_pressed_ab():
+    global speler, Antwoord
+    while begin:
+        speler = C
+    Antwoord = "C"
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global speler, Antwoord
+    while begin:
+        speler = B
+    Antwoord = "B"
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+Antwoord = ""
+speler = 0
+begin = False
+A = 0
+B = 0
+C = 0
 C = 0
 B = 0
 A = 0
-vraag = 0
-operator = True
 Juist = "B"
+Quiz = False
+begin = True
+basic.pause(10000)
+begin = False
+Quiz = True
+Vraag = 0
+punt = 0
+
+while Quiz:
+    Vraag = 1
+    if Vraag == 1:
+        while Vraag == 1:
+            if input.button_is_pressed(Button.B):
+                punt = punt + 1
+                basic.show_string("Goed!")
+                while Vraag == 1:
+                    basic.pause(100)
+            elif input.button_is_pressed(Button.A):
+                basic.show_string("Fout :(")
+                while Vraag == 1:
+                    basic.pause(100)
+    elif Vraag == 2:
+        if input.button_is_pressed(Button.B):
+            punt += 1
+            while Vraag == 2:
+                basic.pause(100)
+        else:
+            while Vraag == 2:
+                basic.pause(100)
+    elif False:
+        pass
+    elif False:
+        pass
+    else:
+        pass
 
 def on_forever():
-    global Juist
-    if vraag > 3:
-        Juist = "A"
-
+    global Vraag
+    basic.pause(5000)
+    Vraag = Vraag + 1
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,14 +1,91 @@
+enum RadioMessage {
+    message1 = 49434,
+    wrong = 39922,
+    right = 32391
+}
 input.onButtonPressed(Button.A, function () {
-    vraag += 1
+    while (begin) {
+        speler = A
+        basic.showString("A")
+        radio.sendString("A")
+    }
 })
-let vraag = 0
-let A = 0
-let B = 0
-let C = 0
-let operator = true
-let Juist = "B"
+radio.onReceivedString(function (receivedString) {
+    if (begin) {
+        if (receivedString == "A") {
+            A = true
+        } else if (receivedString == "B") {
+            B = true
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (begin) {
+        speler = B
+        basic.showString("B")
+        radio.sendString("B")
+    }
+})
+radio.onReceivedMessage(RadioMessage.right, function () {
+    aantal_goed += 1
+    aantal_nog += -1
+})
+radio.onReceivedMessage(RadioMessage.wrong, function () {
+    aantal_nog += -1
+})
+let aantal_nog = 0
+let speler = false
+let aantal_goed = 0
+let score = 0
+let Quiz = false
+let A = false
+let B = false
+let begin = false
+radio.setGroup(50)
+begin = true
+let Vraag = 0
+B = false
+A = false
+if (A && B) {
+    Quiz = true
+    begin = false
+}
+while (Quiz) {
+    let punt = 0
+    if (Vraag <= 3) {
+        while (Vraag <= 3) {
+            if (input.buttonIsPressed(Button.B)) {
+                score += punt - aantal_goed
+                radio.sendMessage(RadioMessage.right)
+                basic.showIcon(IconNames.Yes)
+            } else if (input.buttonIsPressed(Button.A)) {
+                radio.sendMessage(RadioMessage.wrong)
+                basic.showIcon(IconNames.No)
+            }
+        }
+    } else if (Vraag > 4) {
+        Quiz = false
+    } else {
+        if (Vraag == 4) {
+            while (Vraag == 4) {
+                if (input.buttonIsPressed(Button.A)) {
+                    score += punt - aantal_goed
+                    radio.sendMessage(RadioMessage.right)
+                    basic.showIcon(IconNames.Yes)
+                } else if (input.buttonIsPressed(Button.B)) {
+                    radio.sendMessage(RadioMessage.wrong)
+                    basic.showIcon(IconNames.No)
+                }
+            }
+        }
+    }
+}
 basic.forever(function () {
-    if (vraag > 3) {
-        Juist = "A"
+    while (Quiz) {
+        if (aantal_nog == 0) {
+            aantal_nog = 2
+            aantal_goed = 0
+            Vraag += 1
+        }
     }
 })
