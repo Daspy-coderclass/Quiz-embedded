01a8a48d19a9fddeb0fd8b0a8d715cddc5b169b0
Daspy-coderclass <77778303+Daspy-coderclass@users.noreply.github.com>
2021-01-22 11:41:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,46 @@
 enum RadioMessage {
     message1 = 49434,
     wrong = 39922,
-    right = 32391
+    right = 32391,
+    ready = 31336,
+    ready2 = 64034
+}
+function Vraag_1_3 () {
+    if (input.buttonIsPressed(Button.B)) {
+        score += punt - aantal_goed
+        radio.sendMessage(RadioMessage.right)
+        basic.showIcon(IconNames.Yes)
+    } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendMessage(RadioMessage.wrong)
+        basic.showIcon(IconNames.No)
+    }
 }
 input.onButtonPressed(Button.A, function () {
     if (begin) {
         speler = A
         basic.showString("A")
         radio.sendString("A")
+        Aready = true
     }
+})
+radio.onReceivedMessage(RadioMessage.ready, function () {
+    radio.sendMessage(RadioMessage.ready2)
+    begin = false
+    Quiz = true
+})
+radio.onReceivedMessage(RadioMessage.ready2, function () {
+    begin = false
+    Quiz = true
 })
 radio.onReceivedString(function (receivedString) {
     if (begin) {
         if (receivedString == "A") {
-            A = true
+            Aready = true
             Ander_speler = A
         } else if (receivedString == "B") {
-            B = true
+            Bready = true
             Ander_speler = B
+            radio.sendMessage(RadioMessage.ready)
         }
     }
 })
@@ -26,6 +49,7 @@ input.onButtonPressed(Button.B, function () {
         speler = B
         basic.showString("B")
         radio.sendString("B")
+        Bready = true
     }
 })
 radio.onReceivedMessage(RadioMessage.right, function () {
@@ -36,61 +60,50 @@ radio.onReceivedMessage(RadioMessage.wrong, function () {
     aantal_nog += -1
 })
 let aantal_nog = 0
-let Ander_speler = false
-let speler = false
+let B = 0
+let Ander_speler = 0
+let Quiz = false
+let A = 0
+let speler = 0
 let aantal_goed = 0
 let score = 0
-let Quiz = false
-let A = false
-let B = false
+let punt = 0
+let Aready = false
+let Bready = false
 let begin = false
 radio.setGroup(50)
 begin = true
 let Vraag = 0
-B = false
-A = false
-let punt = 2
-while (begin) {
-    if (A && B) {
-        Quiz = true
-        begin = false
-    }
-}
-while (Quiz) {
-    if (Vraag <= 3) {
-        while (Vraag <= 3) {
-            if (input.buttonIsPressed(Button.B)) {
-                score += punt - aantal_goed
-                radio.sendMessage(RadioMessage.right)
-                basic.showIcon(IconNames.Yes)
-            } else if (input.buttonIsPressed(Button.A)) {
-                radio.sendMessage(RadioMessage.wrong)
-                basic.showIcon(IconNames.No)
-            }
-        }
-    } else if (Vraag > 4) {
-        Quiz = false
-    } else {
-        if (Vraag == 4) {
-            while (Vraag == 4) {
-                if (input.buttonIsPressed(Button.A)) {
-                    score += punt - aantal_goed
-                    radio.sendMessage(RadioMessage.right)
-                    basic.showIcon(IconNames.Yes)
-                } else if (input.buttonIsPressed(Button.B)) {
-                    radio.sendMessage(RadioMessage.wrong)
-                    basic.showIcon(IconNames.No)
-                }
-            }
-        }
-    }
-}
+Bready = false
+Aready = false
+punt = 2
 basic.forever(function () {
-    while (Quiz) {
+    if (Quiz) {
         if (aantal_nog == 0) {
             aantal_nog = 2
             aantal_goed = 0
             Vraag += 1
+        }
+    }
+    while (Quiz) {
+        basic.clearScreen()
+        if (Vraag == Vraag + 1) {
+            Vraag_1_3()
+        } else if (Vraag > 4) {
+            Quiz = false
+        } else {
+            if (Vraag == 4) {
+                while (Vraag == 4) {
+                    if (input.buttonIsPressed(Button.A)) {
+                        score += punt - aantal_goed
+                        radio.sendMessage(RadioMessage.right)
+                        basic.showIcon(IconNames.Yes)
+                    } else if (input.buttonIsPressed(Button.B)) {
+                        radio.sendMessage(RadioMessage.wrong)
+                        basic.showIcon(IconNames.No)
+                    }
+                }
+            }
         }
     }
 })
